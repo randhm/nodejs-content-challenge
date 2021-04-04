c16aeb593e51b54b7f1bf8f8c12802954756bc95
Mark Randhawa <mark.randhawa@gmail.com>
2021-04-04 12:09:19 +0100

    refactoring code to use async/await and avoid callback hell. Also modified unit tests

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const md = require('markdown-it')();
-const app = express();
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+const md = require('markdown-it')()
+const util = require('util')
 
-app.get('/*', (req, res) => {
-  fs.readFile(path.join(__dirname, 'content', req.url) + '/index.md','utf8', (err, data) => { 
-    if (err) {
+const readFilePromise = util.promisify(fs.readFile)
+
+app.get('/*', async (req, res) => {
+  try {
+    const markdownData = await readFilePromise(path.join(__dirname, 'content', req.url) + '/index.md', 'utf8')
+    const convertedHTMLData = md.render(markdownData)
+    const originalTemplate = await readFilePromise(path.join(__dirname, '/template.html'), 'utf8')
+    const newTemplate = originalTemplate.replace(/\{\{content\}\}/, convertedHTMLData)
+    res.send(newTemplate)
+  } catch (err) {
       res.status(404).send('Page doesn\'t exist!')
-    } else {
-      const convertedData = md.render(data)
-      fs.readFile(path.join(__dirname, '/template.html'), 'utf8', (err, data) => {
-        data = data.replace(/\{\{content\}\}/, convertedData)
-        res.send(data)
-      })
-    }
-  })
+  } 
 })
 
-app.listen(3000) 
+app.listen(3000)
 
 module.exports = app
--- app.test.js
+++ app.test.js
@@ -1,13 +1,18 @@
-const app = require('./app.js');
-const supertest = require('supertest');
+const app = require('./app.js')
+const supertest = require('supertest')
 const request = supertest(app)
 
-it('Gets the test endpoint and returns a 200 status', async done => {
-  const res = await request.get('/about-page/index.md')
-  expect(res.status).toBe(200)
+it('Gets the test endpoint and returns the correct status code', async done => {
+  const validResponse = await request.get('/valves')
+  expect(validResponse.status).toBe(200)
+  const invalidResponse = await request.get('/another-page')
+  expect(invalidResponse.status).toBe(404)
   done()
 })
 
-/*Test not currently working. As requests are asynchronous, I need to refactor app.js from the callback
-style, to promises and then to the asynchronous style
-*/
+/*Tests are currently dependent on the sub folders within the content folder.
+  To make this work, I would need to create a factory function within app.js that accepts a path
+  argument to the content folder. In my test file, I would need to call the factory function with a path
+  to a new content-test folder, which would never change. This would keep my test seperate from the
+  implementation. I couldn't quite get a factory function to work when I tried it.
+  */
